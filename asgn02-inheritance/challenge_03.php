06bdcc1fe7a2a5467a218707f9bf300489e9ed15
Alex Hamrick <alexhamrick@students.abtech.edu>
2020-09-11 18:30:01 -0400

    Completed the challenge03

--- asgn02-inheritance/challenge_03.php
+++ asgn02-inheritance/challenge_03.php
@@ -1,36 +1,68 @@
 <?php
 
-class Plant {
+class Bicycle {
 
-  var $loves_sun = true;
-	var $made_of_wood = false;
-  var $growth_rate = "grow fast";
-	
-  function r_u_wood() {
-    if ($this->made_of_wood == true){
-			echo "I am made of wood";
-		} 
-		else {
-			echo "I am made not of wood";			
-		}
+  public $brand;
+  public $model;
+  public $year;
+  public $description = 'Used bicycle';
+  private $weight_kg = 0.0;
+  protected $wheels = 2;
+
+  public function name() {
+    return $this->brand . " " . $this->model . " (" . $this->year . ")";
+  }
+
+  public function wheel_details() {
+    $wheel_string = $this->wheels == 1 ? "1 wheel" : "{$this->wheels} wheels";
+    return "It has " . $wheel_string . ".";
+  }
+
+  public function weight_kg() {
+    return $this->weight_kg . ' kg';
+  }
+
+  public function set_weight_kg($value) {
+    $this->weight_kg = floatval($value);
+  }
+
+  public function weight_lbs() {
+    $weight_lbs = floatval($this->weight_kg) * 2.2046226218;
+    return $weight_lbs . ' lbs';
+  }
+
+  public function set_weight_lbs($value) {
+    $this->weight_kg = floatval($value) / 2.2046226218;
   }
 
 }
 
-class Tree extends Plant {
-	var $life_span = "long";
+class Unicycle extends Bicycle {
+  protected $wheels = 1;
 
-var	$made_of_wood = true;
-	
-	function describe_life(){
-		echo "I have a " . $this->life_span . " life span. I also " . $this->growth_rate;
-	}
 }
 
-$pine = new Tree;
+$trek = new Bicycle;
+$trek->brand = 'Trek';
+$trek->model = 'Emonda';
+$trek->year = '2017';
 
+$uni = new Unicycle;
 
-echo $pine->r_u_wood() . "</br>";
-echo $pine->describe_life();
+echo "Bicycle: " . $trek->wheel_details() . "<br />";
+echo "Unicycle: " . $uni->wheel_details() . "<br />";
+echo "<hr />";
+
+echo "Set weight using kg<br />";
+$trek->set_weight_kg(1);
+echo $trek->weight_kg() . "<br />";
+echo $trek->weight_lbs() . "<br />";
+echo "<hr />";
+
+echo "Set weight using lbs<br />";
+$trek->set_weight_lbs(2);
+echo $trek->weight_kg() . "<br />";
+echo $trek->weight_lbs() . "<br />";
+echo "<hr />";
 
 ?>
